04a1d9dbea9f4a7dace3a7d06b2883fd2ecf794f
Alexander Shishkin <alexander.shishckin@gmail.com>
2009-10-25 13:45:52 +0200

    Add makefiles which will replace existing configs with symlinks
    Run "make check" to generate diffs against your local configs.
    Run "make install" to replace your local configs with symlinks
    from this repository.
    
    Signed-off-by: Alexander Shishkin <alexander.shishckin@gmail.com>

---
+++ Makefile
@@ -0,0 +1,18 @@
+BASEDIR := $(CURDIR)
+
+MAKEFLAGS := --no-print-directory
+COMPONENTS := awesome misc mutt vim xsession zsh
+
+default:
+	@echo "run make install to install symlinks"
+
+check:
+	@for c in $(COMPONENTS); do \
+		$(MAKE) check -C $$c BASEDIR=$(BASEDIR); \
+	done
+
+install:
+	@for c in $(COMPONENTS); do \
+		$(MAKE) check -C $$c BASEDIR=$(BASEDIR) DO_INSTALL=1; \
+	done
+
---
+++ rules.mk
@@ -0,0 +1,34 @@
+HOMEDIR := $(HOME)
+
+ifeq ($(DESTDIR),)
+DESTPATH := $(HOMEDIR)/.
+else
+DESTPATH := $(HOMEDIR)/$(DESTDIR)/
+endif
+
+normd = $1
+norms = $(subst $(DESTPATH),$(CURDIR)/,$1)
+DESTFILES := $(patsubst %,$(DESTPATH)%,$(FILES))
+
+$(DESTFILES): $(FILES)
+	@_d=$(call normd,$@); \
+	_s=$(call norms,$@); \
+	echo -n " * $$_s => $$_d: "; \
+	if [ ! -L $$_d ]; then \
+		echo "file"; \
+		if [ -n "$$(diff -ur $$_d $$_s)" ]; then \
+			echo "  ==> differ"; \
+			diff -ur $$_d $$_s > $${_s}.local-diff; \
+		fi; \
+		if [ -n "$(DO_INSTALL)" ]; then \
+			echo "  ==> replacing"; \
+			rm -rf $$_d; ln -sf $$_s $$_d; \
+		fi; \
+	else \
+		echo "symlink"; \
+	fi
+
+check: $(DESTFILES)
+	@echo "Checking commenced. Run git status to see if there are"
+	@echo "any .local-diff files."
+
